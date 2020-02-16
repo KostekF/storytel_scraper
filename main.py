8d0b2a2702f346e745acabe9fefd09b91664eabb
KostekF <filip.kostecki@student.put.poznan.pl>
2020-02-16 23:55:18 +0100

    added dict to store data, code refactoring

--- main.py
+++ main.py
@@ -1,54 +1,81 @@
 import requests
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
 
-storytel_url = 'https://www.storytel.com/pl/pl/books/1167516'
-r = requests.get(storytel_url)
 
-soup = BeautifulSoup(r.text, 'html.parser')
-error_msg = soup.select('.heroContentInner')
 
-if not error_msg:
-    book_wrapper = soup.select_one('#bookDetailWrapper')
+def get_basic_book_data(book_wrapper, book):
+    #Scrape basic book data,
+    #ex. title, author, desc and store it in dict book
     book_title = book_wrapper.select_one('.hide-on-mobile').text.strip()
-    print(book_title)
+    book['title'] = book_title
+    
     book_author = book_wrapper.select_one('.expandAuthorName').text.strip()
-    print(book_author)
+    book['author'] = book_author
 
-    book_voice_over_elem = book_wrapper.select_one('.expandReaderName')
-    
-    
-    if book_voice_over_elem: # ebooks dont have this field
-        book_voice_over = book_voice_over_elem.text.strip()
-    else:
-        book_voice_over = '-'
-    print(book_voice_over)
     
     audiobook_or_ebook = book_wrapper.select_one('.expandReleaseDate').text[:-2].strip()
-    print(audiobook_or_ebook)
+    book['audiobook_or_ebook'] = audiobook_or_ebook
 
     book_description = book_wrapper.select_one('.bookDetailText').text.strip()
-    print(book_description[:50])
+    book['description'] = book_description
     
     book_language = book_wrapper.select_one('.expanderLanguage').text[7:].strip()
-    print(book_language)
+    book['language'] = book_language
 
     book_category = book_wrapper.select_one('.expandCat').text[11:].strip()
-    print(book_category)
+    book['category'] = book_category
+
+def get_additional_book_data(book_wrapper, book):
+    #Scrape additional book data (if it exists),
+    #ex. publisher, release_date, isbn
+
+    book_voice_over_elem = book_wrapper.select_one('.expandReaderName')
+
+    if book_voice_over_elem: # ebooks dont have this field
+        book_voice_over = book_voice_over_elem.text.strip()
+        book['voice_over'] = book_voice_over
 
     book_add_details = book_wrapper.select_one('.publisherInfoContainer')
-
     book_details_elems = book_add_details.select('.bookDetailListItem')
+    
     for i, book_elem in enumerate(book_details_elems, start=1):
         if 'Wydawca' in book_elem.text:
             book_publisher = book_elem.text[9:].strip()
-            print(book_publisher)
+            book['publisher'] = book_publisher
+            
         elif 'Data wydania' in book_elem.text:
             book_release_date = book_elem.text[14:].strip()
-            print(book_release_date)
+            book['release_date'] = book_release_date
+            
         elif 'ISBN' in book_elem.text:
             book_isbn = book_elem.text[7:].strip()
-            print(book_isbn)
+            book['isbn'] = book_isbn
+            
         elif 'Czas trwania' in book_elem.text:
             book_audio_length = book_elem.text[14:].strip()
-            print(book_audio_length)
+            book['audio_length'] = book_audio_length
+            
+def get_book_data(soup, book):
+    #Scrape book data from site
+    book_wrapper = soup.select_one('#bookDetailWrapper')
 
+    get_basic_book_data(book_wrapper, book)
+   
+    get_additional_book_data(book_wrapper, book)
+    
+    
+
+if __name__ == "__main__":
+    storytel_url = 'https://www.storytel.com/pl/pl/books/1167516'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'}
+
+    r = requests.get(storytel_url, headers=headers)
+    open("video.html", "w", encoding='utf8').write(r.text)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    is_page_avail = soup.select('.heroContentInner')
+    if not is_page_avail:
+        book = {}
+        get_book_data(soup, book)
+        print(book)
