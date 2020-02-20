45d2b4d73fd328f642441942e3dcc4d627cdc213
KostekF <filip.kostecki@student.put.poznan.pl>
2020-02-20 23:58:16 +0100

    insert to db all correct data, reject only duplicates/faulty data

--- mySQL_queries.py
+++ mySQL_queries.py
@@ -1,27 +1,27 @@
 create_book_table = (
         '''
-       CREATE TABLE books (
+      CREATE TABLE books (
         book_id int PRIMARY KEY AUTO_INCREMENT, 
         title varchar(200) NOT NULL,
         author varchar(100),
         audiobook_or_ebook varchar(10) NOT NULL,
-        description_ varchar(3000),
+        description_ TEXT,
         language_ varchar(50),
         category varchar(100),
-        voice_over varchar(200),
+        voice_over TEXT,
         publisher varchar(50),
         release_date date,
         ISBN varchar(20),
         audio_length varchar(20),
         page_id varchar(20) UNIQUE,
         created_at timestamp DEFAULT CURRENT_TIMESTAMP
-            );
+     );
         '''
     )
 
 add_book = (
         '''
-        INSERT INTO books 
+        INSERT IGNORE INTO books 
         (title, author, audiobook_or_ebook, description_, language_, 
 		category, voice_over, publisher, release_date, ISBN, audio_length, page_id)
         VALUES (%(title)s, %(author)s, %(audiobook_or_ebook)s, %(description)s,
